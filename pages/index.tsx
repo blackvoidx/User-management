82ef4cf100876e6e4f3d5f127c95597d81d1d348
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-06 16:53:06 +0330

    feat: add container and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Container, Heading } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -9,8 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      </main>
+      <Container maxW="2xl" bg="whiteAlpha.300" paddingTop={'16'} centerContent>
+        <Heading as="h2" size="2xl">User management</Heading>
+      </Container>
     </>
   )
 }
