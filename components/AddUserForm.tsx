1953f008d73bd2c9f59e78e2eac6d8946c417782
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-07 10:20:32 +0330

    feat: initial config for formik

--- components/AddUserForm.tsx
+++ components/AddUserForm.tsx
@@ -1,36 +1,60 @@
 import { Flex, RadioGroup, Radio, Stack, Button } from "@chakra-ui/react"
+import { Form, Formik } from "formik"
 import UserInput from "./UserInput"
 
+interface MyFormValue {
+    firstName: string,
+    lastName: string,
+    email: string,
+    salary: string,
+    birthdayDate: string,
+    status: "Active" | "InActive"
+}
+
 const AddUserForm = () => {
+    const initialValues: MyFormValue = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        salary: "",
+        birthdayDate: "",
+        status: "Active"
+    }
+
     return (
-        <>
-            <Flex
-                marginTop="16"
-                flexWrap='wrap'
-                justifyContent="space-around"
-                alignItems="center"
-                gap="4"
-            >
-                <UserInput label='First name' size='lg' type='text' />
-                <UserInput label='Last name' size='lg' type='text' />
-                <UserInput label='Email' size='lg' type='email' />
-                <UserInput label='Birthday date' size='lg' type='text' />
-                <UserInput label='Salary' size='lg' type='text' />
-                <RadioGroup width="400px" defaultValue='2'>
-                    <Stack spacing={8} direction='row' >
-                        <Radio colorScheme='green' value='1' size="lg">
-                            Active
-                        </Radio>
-                        <Radio colorScheme='red' value='2' size="lg">
-                            InActive
-                        </Radio>
+        <Formik initialValues={initialValues}
+            onSubmit={(values, actions) => {
+                console.log({ values, actions });
+            }}>
+            <Form>
+                <Flex
+                    marginTop="16"
+                    flexWrap='wrap'
+                    justifyContent="space-around"
+                    alignItems="center"
+                    gap="4"
+                >
+                    <UserInput name="firstName" label='First name' />
+                    <UserInput name="lastName" label='Last name' />
+                    <UserInput name="email" label='Email' />
+                    <UserInput name="birthdayDate" label='Birthday date' />
+                    <UserInput name="salary" label='Salary' />
+                    <RadioGroup width="400px" defaultValue='2'>
+                        <Stack spacing={8} direction='row' >
+                            <Radio colorScheme='green' value='1' size="lg">
+                                Active
+                            </Radio>
+                            <Radio colorScheme='red' value='2' size="lg">
+                                InActive
+                            </Radio>
+                        </Stack>
+                    </RadioGroup>
+                    <Stack width="full" paddingX="4">
+                        <Button type="submit" width="300px" marginTop="4" colorScheme='whatsapp'>Add User</Button>
                     </Stack>
-                </RadioGroup>
-                <Stack width="full" paddingX="4">
-                    <Button width="300px" marginTop="4" colorScheme='whatsapp'>Add User</Button>
-                </Stack>
-            </Flex>
-        </>
+                </Flex >
+            </Form>
+        </Formik>
     )
 }
 
--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -1,18 +1,18 @@
 import { FormControl, FormLabel, Input, FormHelperText } from "@chakra-ui/react"
-import { FC } from "react"
-import { InputProps } from "@chakra-ui/react"
+import { useField } from "formik"
 
-type InputCustomProps = {
-    label: string
+interface ComponentProps<T> {
+    label: string;
+    name: string;
 }
 
-type InputChakraProps = InputProps & InputCustomProps
-
-const UserInput: FC<InputChakraProps> = (props) => {
+const UserInput = (props: ComponentProps<string>) => {
+    const [field, meta] = useField(props)
+    console.log(field, meta)
     return (
-        <FormControl width="400px">
+        <FormControl width="400px" isInvalid={meta.touched && !!meta.error}>
             <FormLabel>{props.label}</FormLabel>
-            <Input type='email' size={props.size} />
+            <Input size='lg' type='text' {...field} />
             {/* <FormHelperText>Email not valid</FormHelperText> */}
         </FormControl>
     )
