81fb9a61296385599eabc2ca0ab5a7bef8a58ff9
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-01-25 12:04:17 +0330

    fix: production url

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://user-management-kappa.vercel.app",
+    baseUrl: process.env.PROD_URL,
   }),
   endpoints: (builder) => ({}),
   tagTypes: ["User"],
